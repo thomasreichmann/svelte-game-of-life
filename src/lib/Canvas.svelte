e86973f9419a78fb9248faf5b7d10353ef5d3f43
Thomas <thomasreichmann@hotmail.com>
2024-01-28 19:03:53 -0300

    .

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -2,55 +2,176 @@
   import {onMount} from "svelte";
   import {Board, type BoardState} from "./game/Game";
 
-  const render = (ctx: CanvasRenderingContext2D, state: BoardState, scale: number) => {
-    for (let x = 0; x < state.width; x++) {
-      for (let y = 0; y < state.height; y++) {
-        let cell = state.getCell(x, y)
-
-        if (cell) ctx.fillStyle = '#e78d06'
-        else ctx.fillStyle = '#000'
-
-        ctx.fillRect(x * scale, y * scale, scale, scale)
-      }
-    }
-  }
-
   let canvas: HTMLCanvasElement;
   let board: Board;
-  let width = 50
-  let height = 50
-  let scale = 6
+  let scale = 20;
+  let isDragging = false;
+  let isPanning = false;
+  let play = false;
+  let frameLoopId: number;
+  let frameSpeed = 75;
+  let currentToggleState: boolean | null = null;
+  let viewport = {x: 0, y: 0};
+
+  let cellCount = 0
+
+  const render = (ctx: CanvasRenderingContext2D, state: BoardState, scale: number) => {
+    if (!canvas) return;
+
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    state.cells.forEach((val, key) => {
+      const [x, y] = key.split(',').map(Number);
+      let cell = state.getCell(x, y);
+      if (cell) ctx.fillStyle = '#e78d06';
+      else ctx.fillStyle = '#000';
+      ctx.fillRect((x - viewport.x) * scale, (y - viewport.y) * scale, scale, scale);
+    });
+
+    cellCount = state.cells.size
+  };
+
+  const toggleCellAt = (event: MouseEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) / scale) + viewport.x;
+    const y = Math.floor((event.clientY - rect.top) / scale) + viewport.y;
+    if (currentToggleState === null) {
+      const currentCellState = board.getCell(x, y);
+      currentToggleState = !currentCellState;
+    }
+    board.setCellByCardinal(x, y, currentToggleState);
+  };
+
+  const handleMouseDown = (event: MouseEvent) => {
+    if (event.shiftKey) {
+      isPanning = true;
+      canvas.style.cursor = 'move';
+    } else {
+      isDragging = true;
+      currentToggleState = null;
+      toggleCellAt(event);
+    }
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isPanning) {
+      viewport.x -= event.movementX / scale;
+      viewport.y -= event.movementY / scale;
+      requestAnimationFrame(() => board.render());  // Trigger board render
+    } else if (isDragging) {
+      toggleCellAt(event);
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (isPanning) {
+      // Snap viewport position to the nearest grid point after panning
+      viewport.x = Math.round(viewport.x);
+      viewport.y = Math.round(viewport.y);
+
+      requestAnimationFrame(() => board.render()); // Update the rendering after snapping
+    }
+
+    isDragging = false;
+    isPanning = false;
+    canvas.style.cursor = 'default';
+    currentToggleState = null;
+  };
+
+  onMount(() => {
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    board = new Board((state) => render(ctx, state, scale));
+  });
 
   const tick = () => {
     if (board) {
-      board.tick()
+      board.tick();
     }
+  };
+
+  const togglePlay = () => {
+    play = !play;
+  };
+
+  $: if (play) {
+    clearInterval(frameLoopId);
+    frameLoopId = setInterval(tick, frameSpeed);
+  } else {
+    clearInterval(frameLoopId);
   }
 
-  onMount(() => {
-    let ctx = canvas.getContext('2d')
+  const handleWheel = (event: WheelEvent) => {
+    if (event.shiftKey) {
+      event.preventDefault();
+
+      const rect = canvas.getBoundingClientRect();
+
+      // Calculate the cursor position relative to the canvas before scaling
+      const cursorXBeforeScale = (event.clientX - rect.left) / scale + viewport.x;
+      const cursorYBeforeScale = (event.clientY - rect.top) / scale + viewport.y;
+
+      // Determine the scale factor based on the scroll direction
+      const scaleFactor = event.deltaY < 0 ? 1 : -1;
+      scale = Math.max(1, scale + scaleFactor)
+
+      // Adjust the viewport based on the cursor's position
+      viewport.x = Math.round(cursorXBeforeScale - (event.clientX - rect.left) / scale);
+      viewport.y = Math.round(cursorYBeforeScale - (event.clientY - rect.top) / scale);
+
+      // Render the board with the new scale
+      requestAnimationFrame(() => board.render());
+    }
+  };
+
+  const resetVp = () => {
+    viewport.x = 0
+    viewport.y = 0
+
+    scale = 20
+
+    requestAnimationFrame(() => board.render());
+  }
+
+  const reset = () => {
+    resetVp()
+    const ctx = canvas.getContext('2d');
     if (!ctx) return;
-
-
-    board = new Board(width, height, (state) => render(ctx!, state, scale))
-
-    // board.setCellByCardinal(1, 1, true)
-    // board.setCellByCardinal(1, 2, true)
-    // board.setCellByCardinal(1, 3, true)
-
-    board.tick()
-  })
-
-  const handleSliderChange = (event: any) => {
-    scale = event.target.value
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    play = false
+    board = new Board((state) => render(ctx, state, scale));
   }
 </script>
 
 <div class="container">
-    <div>
+    <div style="display: flex; gap: 12px">
         <button on:click={tick}>tick</button>
+        <button on:click={togglePlay}>play/pause</button>
+        <button on:click={reset}>reset</button>
+        <button on:click={resetVp}>view port 0</button>
+        <div style="display: flex; flex-direction: column">
+            <span>cells: {cellCount}</span>
+            <span>scale: {scale}</span>
+        </div>
+        <div style="display: flex; flex-direction: column">
+            <label for="frame-speed">Frame Speed: {frameSpeed}</label>
+            <input aria-label="simulation speed (ms)" bind:value={frameSpeed} id="frame-speed" max="300" min="1"
+                   type="range">
+        </div>
+        <span>{play ? "playing" : "paused"}</span>
     </div>
-    <canvas bind:this={canvas} height={height * scale} width={width * scale}/>
+    <canvas
+            bind:this={canvas}
+            height={800}
+            on:mousedown={handleMouseDown}
+            on:mouseleave={handleMouseUp}
+            on:mousemove={handleMouseMove}
+            on:mouseup={handleMouseUp}
+            on:wheel={handleWheel}
+            width={1500}
+    />
 </div>
 
 <style>
@@ -59,6 +180,8 @@
         flex-direction: column;
         align-items: center;
         gap: 1em;
+        height: 100vh;
+        width: 100vh;
     }
 
     button {
@@ -67,5 +190,6 @@
 
     canvas {
         border: 2px solid #ff66d0;
+        cursor: default;
     }
 </style>
--- src/lib/game/Game.ts
+++ src/lib/game/Game.ts
@@ -3,123 +3,79 @@ interface IBoard {
 }
 
 export interface BoardState {
-  width: number;
-  height: number;
-  cells: Cell[];
-  getCell: (x: number, y: number) => Cell | undefined;
+  cells: Map<string, boolean>;
+  getCell: (x: number, y: number) => boolean;
+  indexToCardinal: (index: string) => { x: number, y: number };
 }
 
-type Cell = boolean;
-
 export class Board implements IBoard {
-  cells: Cell[] = [];
+  public cells: Map<string, boolean> = new Map();
+  private changedCells: Set<string> = new Set();
 
-  constructor(
-    private width: number,
-    private height: number,
-    public onRender?: (a: BoardState) => void,
-  ) {
-    this.cells = this.createCells();
+  constructor(public onRender?: (state: BoardState) => void) {
+    this.setCellByCardinal(4, 3, true);
+    this.setCellByCardinal(3, 4, true);
+    this.setCellByCardinal(4, 4, true);
+    this.setCellByCardinal(5, 4, true);
   }
 
   public tick() {
-    const newState: Cell[] = [...this.cells]
-    //
-    // for (let i = 0; i < this.cells.length; i++) {
-    //   let neighbors = this.getNeighbors(i)
-    //
-    //   let aliveCount = neighbors.filter(e => e).length
-    //   let deadCount = neighbors.filter(e => !e).length
-    //
-    //   if (this.cells[i]) {
-    //     // Checks to do if the cell is alive
-    //     if (aliveCount < 2) {
-    //       // Dies of underpopulation
-    //       newState[i] = false
-    //     } else if (aliveCount <= 3) {
-    //       // Continues to next gen
-    //     } else {
-    //       // Dies of overpopulation
-    //       newState[i] = false
-    //     }
-    //   } else {
-    //     // Check to do if the cell is dead
-    //     if (aliveCount == 3) {
-    //       newState[i] = true
-    //     }
-    //   }
-    // }
+    let newState = new Map<string, boolean>();
+    let cellsToCheck = new Set<string>([...this.cells.keys()].flatMap(key => this.getNeighbors(key)));
 
-    this.cells = newState
+    cellsToCheck.forEach(key => {
+      let aliveCount = this.getNeighbors(key).filter(neighborKey => this.cells.get(neighborKey)).length;
+      let isAlive = this.cells.get(key) || false;
+
+      if (isAlive && (aliveCount < 2 || aliveCount > 3)) {
+        // Cell dies, do not add to newState
+      } else if (!isAlive && aliveCount === 3) {
+        newState.set(key, true); // Cell becomes alive
+      } else if (isAlive) {
+        newState.set(key, true); // Keep alive cell
+      }
+    });
+
+    this.cells = newState;
 
     if (this.onRender) {
       this.onRender(this.getBoardState());
     }
   }
 
-  public getNeighbors(i: number): boolean[] {
-    let {x, y} = this.indexToCardinal(i)
-
-    const t = (x: number, y: number) => this.cells[this.cardinalToIndex(x, y)]
-
-    let topLeft = t(x - 1, y - 1)
-    let topMiddle = t(x, y - 1)
-    let topRight = t(x - 1, y)
-
-    let middleLeft = t(x - 1, y)
-    let middleRight = t(x + 1, y)
-
-    let bottomLeft = t(x - 1, y + 1)
-    let bottomMiddle = t(x, y + 1)
-    let bottomRight = t(x + 1, y + 1)
-
-    return [topLeft, topMiddle, topRight, middleLeft, middleRight, bottomLeft, bottomMiddle, bottomRight]
+  public getNeighbors(key: string): string[] {
+    const [x, y] = key.split(',').map(Number);
+    return [
+      `${x - 1},${y - 1}`, `${x},${y - 1}`, `${x + 1},${y - 1}`,
+      `${x - 1},${y}`, `${x + 1},${y}`,
+      `${x - 1},${y + 1}`, `${x},${y + 1}`, `${x + 1},${y + 1}`
+    ];
   }
 
-  public getCell(x: number, y: number): Cell | undefined {
-    return this.cells[this.cardinalToIndex(x, y)];
-  }
-
-  public cardinalToIndex(x: number, y: number): number {
-    return this.width * y + x;
-  }
-
-  public indexToCardinal(index: number): { x: number, y: number } {
-    let x = Math.floor(index / this.width);
-    let y = index % this.width;
-    return {x, y};
-  }
-
-  public setCell(index: number, value: boolean) {
-    if (index > this.cells.length - 1) return;
-
-    this.cells[index] = value
+  public getCell(x: number, y: number): boolean {
+    return this.cells.get(`${x},${y}`) || false;
   }
 
   public setCellByCardinal(x: number, y: number, value: boolean) {
-    let index = this.cardinalToIndex(x, y);
-
-    // Check if passed x,y are within array bounds
-    if (this.cells.length - 1 < index) return;
-
-    this.cells[index] = value;
+    const key = `${x},${y}`;
+    this.cells.set(key, value);
+    this.changedCells.add(key);
+    this.render();
   }
 
   public getBoardState(): BoardState {
     return {
-      width: this.width,
-      height: this.height,
       cells: this.cells,
       getCell: this.getCell.bind(this),
+      indexToCardinal: (key: string) => {
+        const [x, y] = key.split(',').map(Number);
+        return {x, y};
+      }
     };
   }
 
-  private createCells(): boolean[] {
-    // return Array.from({length: this.width * this.height}, () => Math.random() < 0.5);
-    return new Array(this.width * this.height).fill(true);
-  }
-
-  private render() {
+  public render() {
     this.onRender?.(this.getBoardState());
+    this.changedCells.clear();
   }
 }
